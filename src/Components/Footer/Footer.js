b3ce8d2c4499b50eefad1ae93434fe488d4a9498
Gabriel <gabrielalcantara1823@gmail.com>
2022-09-29 12:49:36 -0300

    Footer done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import * as S from './Style'
+import FB from '../data/Desktop/FB.png'
+import TT from '../data/Desktop/TT.png'
+import INSTA from '../data/Desktop/INSTA.png'
+import PINT from '../data/Desktop/PINT.png'
 
 const Footer = () => {
     return(
-        <></>
+        <>
+        <S.MomContainer>
+            <S.ImageContainer>
+                <S.Image src={INSTA} alt='Instagram Logo'/>
+                <S.Image src={TT} alt='Twitter Logo'/>
+                <S.Image src={FB} alt='Facebook Logo'/>
+                <S.Image src={PINT} alt='Pinterest Logo'/>
+            </S.ImageContainer>
+            <S.MenuContainer>
+                <S.DesktopMenu>
+                    <S.Menu>ABOUT</S.Menu>
+                    <S.Menu>RECIPES</S.Menu>
+                    <S.Menu>SUBSCRIBE</S.Menu>
+                </S.DesktopMenu>
+            </S.MenuContainer>
+        </S.MomContainer>
+        <S.CreditsContainer>
+            <S.Credits>Layout produzido por Vai na Web para fins exclusivamente educacionais. Referência: https://br.pinterest.com/pin/AVuDlMAl4GsQiM6nijH9YbG9bsNKpompSEOEHzig6GJ58AnUtMkSy7k/</S.Credits>
+        </S.CreditsContainer>
+        </>
     )
 }
 
--- src/Components/Footer/Style.js
+++ src/Components/Footer/Style.js
@@ -1,5 +1,123 @@
 import styled from 'styled-components'
 
 export const MomContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #F2F4F1;
+    padding-right: 5vw;
+    padding-left: 5vw;
+    height: 250px;
+    @media screen and (max-width: 1024px){
+        height: 180px;
+    }
+    @media screen and (max-width: 768px){
+        flex-direction: column-reverse;
+        justify-content: center;
+        height: 350px;
+    }
+    @media screen and (max-width: 375px){
+        height: 300px;
+    }
+`
 
+export const ImageContainer = styled.div`
+    display: flex;
+    width: 20%;
+    justify-content: space-between;
+    @media screen and (max-width: 1024px){
+        width: 23%;
+    }
+    @media screen and (max-width: 768px){
+        width: 50%;
+    }
+    @media screen and (max-width: 425px){
+        width: 75%;
+    }
+    @media screen and (max-width: 375px){
+        width: 80%;
+    }
+`
+
+export const Image = styled.img`
+    width: 3.5vw;
+    @media screen and (max-width: 1024px){
+        width: 4vw;
+    }
+    @media screen and (max-width: 768px){
+        width: 6.7vw;
+    }
+    @media screen and (max-width: 425px){
+        width: 12vw;
+    }
+    @media screen and (max-width: 375px){
+        width: 13vw;
+    }
+`
+
+export const MenuContainer = styled.div`
+    width: 30%;
+    height: 40%;
+    @media screen and (max-width: 1024px){
+        width: 34%;
+    }
+    @media screen and (max-width: 768px){
+        width: 50%;
+        margin-bottom: 2.5vw;
+    }
+    @media screen and (max-width: 425px){
+        width: 75%;
+    }
+`
+
+export const DesktopMenu = styled.ul`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    @media screen and (max-width: 768px){
+        flex-direction: column;
+        justify-content: space-evenly;
+        height: 100%;
+        align-items: center;
+    }
+`
+
+export const Menu = styled.li`
+    color: #373737;
+    font-size: 1.2vw;
+    @media screen and (max-width: 1024px){
+        font-size: 1.6vw;
+    }
+    @media screen and (max-width: 768px){
+        font-size: 2.5vw;
+    }
+    @media screen and (max-width: 425px){
+        font-size: 5vw;
+    }
+    @media screen and (max-width: 375px){
+        font-size: 5.5vw;
+    }
+`
+
+export const CreditsContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #446061;
+    height: 50px;
+    @media screen and (max-width: 425px){
+        height: 35px;
+    }
+`
+
+export const Credits = styled.p`
+    color: #FFFFFF;
+    opacity: 0.5;
+    font-size: 1.1vw;
+    @media screen and (max-width: 425px){
+        font-size: 2vw;
+    }
+    @media screen and (max-width: 375x){
+        font-size: 2.5vw;
+    }
 `
